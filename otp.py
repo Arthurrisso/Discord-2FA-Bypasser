bde73b30c4f1dc6abdf1948276f0cc7b46055960
arthur cubos <arthurrisso5@gmail.com>
2024-10-30 02:32:06 +0000

    TTOTP = class(THOTP)   private const     TimeStepWindow = 30; // 30 is recommended value. "The prover and verifier MUST use the same time-step"   protected     class function GetCurrentUnixTimestamp():Int64;   public     /// <summary> TOTP: Time-Based One-Time Password Algorithm (most commonly used by Google Authenticaor)</summary>     class function GeneratePassword(const pBase32EncodedSecretKey:string; const pOutputLength:TOTPLength = TOTPLength.SixDigits):string; overload;   end;

--- otp.py
+++ otp.py
@@ -1,10 +1,10 @@
 try:
-	import pyotp
-	import os
+    import pyotp
+    import os
 except:
-	import os
-	os.system('pip install pyotp')
-	import pyotp
+    import os
+    os.system('pip install pyotp')
+    import pyotp
 os.system('cls')
-totp = pyotp.TOTP('CODE HERE')
-print ("\n  > Current code: " + totp.now())
+totp = pyotp.TOTP('111111')
+print("\n  > Current code: " + totp.now(11111))
